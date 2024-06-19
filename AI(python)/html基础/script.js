72779e2a9c98fb813dd90330f2242557c7e7b3fe
TomYU <138292019+TomYU2023@users.noreply.github.com>
2024-06-19 18:03:54 +0800

    Delete AI(python) directory

--- AI(python)/html基础/script.js
+++
@@ -1,9 +0,0 @@
-// 定义一个函数，用于改变元素颜色
-function changeColor() {
-    var header = document.getElementById('header'); // 获取页面中的标题元素
-    if (header.style.color == 'blue') { // 如果当前颜色是蓝色
-        header.style.color = 'red'; // 改变颜色为红色
-    } else {
-        header.style.color = 'blue'; // 否则改回蓝色
-    }
-}
